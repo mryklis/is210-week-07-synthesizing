e61b9f2964a280208e794f9e0baf0b1ddd21a4cb
maxim <maxim.ryklis@spsmail.cuny.edu>
2016-10-12 13:35:14 -0400

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -1,4 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module creates a matchup list of players"""
+
+
 def get_matches(players):
+	"""
+	
+	Args:
+	    players (tuple): 
+	    
+	Returns:
+	    matchup (list): 
+	    
+	Example:
+	    >>> get_matches(['Harry', 'Howard', 'Hugh'])
+	    [('Harry', 'Howard'), ('Harry', 'Hugh'), ('Howard', 'Hugh')]
+	"""
 	matchup = []
 	for indx1, player1 in enumerate(players):
 		for indx2, player2 in enumerate(players):
