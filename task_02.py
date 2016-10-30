0cc41deae294b899007e34dcf4e9bbc0560b8f9f
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-10-30 16:52:34 +0000

    task_02 compelte

--- task_02.py
+++ task_02.py
@@ -5,6 +5,18 @@
 import authentication
 import getpass
 
-def login(username, maxattempts):
-	authd = False
-	while 
+def login(username, maxattempts=3):
+    authd = False
+    while maxattempts != 0:
+        password = getpass.getpass('Please enter your password:')
+        authd = authentication.authenticate(username, password)
+        if authd is False:
+            maxattempts -= 1
+            attempts = 'Incorrect credentials, You have ' + \
+                       str(maxattempts) + ' attempts left.'
+            print attempts
+            if maxattempts == 0:
+                return authenticated
+        elif authd is True:
+            return authd == True
+	
